7cb5b26725ae3b8f8ad9be15fa74978b480393c0
pikesley <github@orgraphone.org>
2016-01-08 11:45:26 +0000

    Descriptions

--- lib/banjaxer.rb
+++ lib/banjaxer.rb
@@ -10,11 +10,13 @@ module Banjaxer
     end
     map %w(-v --version) => :version
 
+    desc 'get_url', 'GET a url and tell us the Content-Length'
     def get_url url
       h = HTTParty.get url, headers: { 'Accept' => 'application/json' }
       puts "Content-Length is #{h.headers['Content-Length']}"
     end
 
+    desc 'tell_the_time', 'Tell us the current time and date'
     def tell_the_time
       puts Time.new.strftime "%H:%M on %A %B %-d, %Y"
     end

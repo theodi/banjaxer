820ee2029df6840637dac7a6a3624f8ca19fbfae
pikesley <github@orgraphone.org>
2016-01-08 16:51:22 +0000

    Tighter regexes

--- spec/banjaxer_spec.rb
+++ spec/banjaxer_spec.rb
@@ -5,16 +5,16 @@ module Banjaxer
     end
 
     it 'has a version' do
-      expect { subject.version }.to output(/banjaxer version #{VERSION}/).to_stdout
+      expect { subject.version }.to output(/^banjaxer version #{VERSION}$/).to_stdout
     end
 
     it 'gets the url', :vcr do
-      expect { subject.get_url 'http://uncleclive.herokuapp.com/banjax' }.to output(/Content-Length is 808/).to_stdout
+      expect { subject.get_url 'http://uncleclive.herokuapp.com/banjax' }.to output(/^Content-Length is 808$/).to_stdout
     end
 
     it 'knows what time it is' do
       Timecop.freeze Time.local 1974, 06, 15, 9, 30 do
-        expect { subject.tell_the_time }.to output(/09:30 on Saturday June 15, 1974/).to_stdout
+        expect { subject.tell_the_time }.to output(/^09:30 on Saturday June 15, 1974$/).to_stdout
       end
     end
 
@@ -30,12 +30,12 @@ module Banjaxer
 
     context 'with options' do
       it 'is fine with no options' do
-        expect { subject.embiggen 'Springfield' }.to output(/embiggening Springfield/).to_stdout
+        expect { subject.embiggen 'Springfield' }.to output(/^embiggening Springfield$/).to_stdout
       end
 
       it 'can handle an option' do
         subject.options = {json: true}
-        expect { subject.embiggen 'Springfield' }.to output(/{"embiggening":"Springfield"}/).to_stdout
+        expect { subject.embiggen 'Springfield' }.to output(/^{"embiggening":"Springfield"}$/).to_stdout
       end
     end
   end
